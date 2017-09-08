782af30533ccf047bc80d554393dd46cef969a88
nguyendac8 <congdacit@gmail.com>
2017-09-08 02:46:11 -0400

    change /logout go GET instead of DELETE in routes  and update link_to in application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get get'/logout', to: 'sessions#destroy'
   resources :users do
     member do
       get :following, :followers
